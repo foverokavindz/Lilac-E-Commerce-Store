b9594c4dd1cbbbdaa06f78af956d919a5e72e4fe
Kavinda Maduranga <kavindamadhuranga74@gmail.com>
2023-12-31 11:09:08 +0530

    add profile page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const headerStyle = {
   backgroundColor: '#DADADA',
@@ -7,6 +8,7 @@ const headerStyle = {
 };
 
 const Navbar = () => {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div style={headerStyle}>
       <div>
@@ -24,9 +26,18 @@ const Navbar = () => {
             {' '}
             <li>About</li>
           </Link>
-          <Link to={'/sign-in'}>
-            {' '}
-            <li>Sign in</li>
+          <Link to={'/profile'}>
+            {currentUser ? (
+              // eslint-disable-next-line jsx-a11y/img-redundant-alt
+              <img
+                src={currentUser.profilePicture}
+                alt={'profile image'}
+                width={'80px'}
+                style={{ borderRadius: '100%' }}
+              />
+            ) : (
+              <li>Sign in</li>
+            )}
           </Link>
         </ul>
       </div>
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,64 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  const handleChnage = () => {};
+  return (
+    <div>
+      <form>
+        <img
+          src={currentUser.profilePicture}
+          alt="profile picture"
+          width={'100px'}
+        />
+        <p> {currentUser.email}</p>
+
+        <input
+          type="text"
+          placeholder="firstname"
+          id="firstName"
+          className=""
+          onChange={handleChnage}
+          defaultValue={currentUser.firstName}
+        />
+        <input
+          type="text"
+          placeholder="lastname"
+          id="lastName"
+          className=""
+          onChange={handleChnage}
+          defaultValue={currentUser.lastName}
+        />
+        <input
+          type="text"
+          placeholder="phone"
+          id="phone"
+          className=""
+          onChange={handleChnage}
+          defaultValue={currentUser.phone}
+        />
+        <input
+          type="text"
+          placeholder="address"
+          id="address"
+          className=""
+          onChange={handleChnage}
+          defaultValue={currentUser.address}
+        />
+        <input
+          type="text"
+          placeholder="city"
+          id="city"
+          className=""
+          onChange={handleChnage}
+          defaultValue={currentUser.city}
+        />
+
+        <button>Update</button>
+      </form>
+    </div>
+  );
 };
 
 export default Profile;
