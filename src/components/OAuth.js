5dce893e8fc1d3515e970fccdbd60b4c2b998046
Kavinda Maduranga <kavindamadhuranga74@gmail.com>
2024-01-07 10:49:46 +0530

    fix a bug

--- src/components/OAuth.js
+++ src/components/OAuth.js
@@ -45,7 +45,7 @@ const OAuth = () => {
       <button
         onClick={handleGoogleClick}
         type="button"
-        class=" px-5 py-4  w-full text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2"
+        class=" px-5 py-4  w-full text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm  text-center items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2"
       >
         <svg
           class="w-4 h-4 me-2 inline-flex"
