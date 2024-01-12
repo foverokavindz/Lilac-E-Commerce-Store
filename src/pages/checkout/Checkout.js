a2d0e875abfe0ceed6cdcb49c5881f85b63aa699
Kavinda Maduranga <kavindamadhuranga74@gmail.com>
2024-01-13 02:03:07 +0530

    connect checkout page with react and designed that page

--- src/pages/checkout/Checkout.js
+++ src/pages/checkout/Checkout.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import {
+  addItemToCart,
+  removeItemFromCart,
+  removeOneProduct,
+} from '../../store/reducers/cart/cartSlice';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const Checkout = () => {
+  const dispatch = useDispatch();
+  const { items, subtotal, totalQuantity } = useSelector((state) => state.cart);
+  const { currentUser } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+
   return (
     <div className="container mx-auto max-w-7xl py-16 flex flex-col">
       <div class="md:px-6 ">
@@ -14,100 +26,58 @@ const Checkout = () => {
         </div>
         <div class="mt-10 flex flex-col xl:flex-row jusitfy-center items-stretch w-full xl:space-x-8 space-y-4 md:space-y-6 xl:space-y-0">
           <div class="flex flex-col justify-start items-start w-full space-y-4 md:space-y-6 xl:space-y-8">
-            <div class="flex flex-col justify-start items-start  bg-gray-50 px-4 py-4 md:py-6 md:p-6 xl:p-8 w-full">
-              <p class="text-lg md:text-xl  font-semibold leading-6 xl:leading-5 text-gray-800">
-                Customer’s Cart
+            <div class="flex flex-col justify-start items-start rounded-3xl bg-gray-50 px-4 py-4 md:py-6 md:p-6 xl:p-8 w-full">
+              <p class="text-lg md:text-xl  font-semibold leading-6 xl:leading-5 text-gray-800 mb-6">
+                {currentUser.firstName}’s Cart
               </p>
-              <div class="mt-4 md:mt-6 flex flex-col md:flex-row justify-start items-start md:items-center md:space-x-6 xl:space-x-8 w-full">
-                <div class="pb-4 md:pb-8 w-full md:w-40">
-                  <img
-                    class="w-full hidden md:block"
-                    src="https://i.ibb.co/84qQR4p/Rectangle-10.png"
-                    alt="dress"
-                  />
-                  <img
-                    class="w-full md:hidden"
-                    src="https://i.ibb.co/L039qbN/Rectangle-10.png"
-                    alt="dress"
-                  />
-                </div>
-                <div class="border-b border-gray-200 md:flex-row flex-col flex justify-between items-start w-full pb-8 space-y-4 md:space-y-0">
-                  <div class="w-full flex flex-col justify-start items-start space-y-8">
-                    <h3 class="text-xl  xl:text-2xl font-semibold leading-6 text-gray-800">
-                      Premium Quaility Dress
-                    </h3>
-                    <div class="flex justify-start items-start flex-col space-y-2">
-                      <p class="text-sm  leading-none text-gray-800">
-                        <span class=" text-gray-300">Style: </span> Italic
-                        Minimal Design
-                      </p>
-                      <p class="text-sm  leading-none text-gray-800">
-                        <span class=" text-gray-300">Size: </span> Small
-                      </p>
-                      <p class="text-sm  leading-none text-gray-800">
-                        <span class=" text-gray-300">Color: </span> Light Blue
-                      </p>
+
+              {items.map((product) => {
+                return (
+                  <div class="mt-4 md:mt-6 flex flex-col md:flex-row justify-start items-start md:items-center md:space-x-6 xl:space-x-8 w-full">
+                    {/* product Item */}
+                    <div class="pb-4 md:pb-8 w-full md:w-40">
+                      <img
+                        class="w-32 h-32 rounded-2xl"
+                        src={product.image}
+                        alt="dress"
+                      />
+                    </div>
+
+                    <div class="border-b border-gray-200 md:flex-row flex-col flex justify-between items-start w-full pb-8 space-y-4 md:space-y-0">
+                      <div class="w-full flex flex-col justify-start items-start space-y-8">
+                        <h3 class="text-xl  xl:text-2xl font-semibold leading-6 text-gray-800">
+                          {product.name}
+                        </h3>
+                        <div class="flex justify-start items-start flex-col space-y-2">
+                          <p class="text-sm  leading-none text-gray-700">
+                            <span class=" text-gray-500">Size: </span>{' '}
+                            {product.selectedSize}
+                          </p>
+                          <p class="text-sm  leading-none text-gray-700">
+                            <span class=" text-gray-500">Color: </span>
+                            {product.selectedColor}
+                          </p>
+                        </div>
+                      </div>
+                      <div class="flex justify-between space-x-8 items-start w-full">
+                        <p class="text-base  xl:text-lg leading-6">
+                          ${product.price}{' '}
+                          <span class="text-red-300 line-through">
+                            {' '}
+                            ${product.price}
+                          </span>
+                        </p>
+                        <p class="text-base  xl:text-lg leading-6 text-gray-800">
+                          {product.quantity}
+                        </p>
+                        <p class="text-base  xl:text-lg font-semibold leading-6 text-gray-800">
+                          ${product.price}{' '}
+                        </p>
+                      </div>
                     </div>
                   </div>
-                  <div class="flex justify-between space-x-8 items-start w-full">
-                    <p class="text-base  xl:text-lg leading-6">
-                      $36.00{' '}
-                      <span class="text-red-300 line-through"> $45.00</span>
-                    </p>
-                    <p class="text-base  xl:text-lg leading-6 text-gray-800">
-                      01
-                    </p>
-                    <p class="text-base  xl:text-lg font-semibold leading-6 text-gray-800">
-                      $36.00
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div class="mt-6 md:mt-0 flex justify-start flex-col md:flex-row items-start md:items-center space-y-4 md:space-x-6 xl:space-x-8 w-full">
-                <div class="w-full md:w-40">
-                  <img
-                    class="w-full hidden md:block"
-                    src="https://i.ibb.co/s6snNx0/Rectangle-17.png"
-                    alt="dress"
-                  />
-                  <img
-                    class="w-full md:hidden"
-                    src="https://i.ibb.co/BwYWJbJ/Rectangle-10.png"
-                    alt="dress"
-                  />
-                </div>
-                <div class="flex justify-between items-start w-full flex-col md:flex-row space-y-4 md:space-y-0">
-                  <div class="w-full flex flex-col justify-start items-start space-y-8">
-                    <h3 class="text-xl  xl:text-2xl font-semibold leading-6 text-gray-800">
-                      High Quaility Italic Dress
-                    </h3>
-                    <div class="flex justify-start items-start flex-col space-y-2">
-                      <p class="text-sm  leading-none text-gray-800">
-                        <span class=" text-gray-300">Style: </span> Italic
-                        Minimal Design
-                      </p>
-                      <p class="text-sm  leading-none text-gray-800">
-                        <span class=" text-gray-300">Size: </span> Small
-                      </p>
-                      <p class="text-sm  leading-none text-gray-800">
-                        <span class=" text-gray-300">Color: </span> Light Blue
-                      </p>
-                    </div>
-                  </div>
-                  <div class="flex justify-between space-x-8 items-start w-full">
-                    <p class="text-base  xl:text-lg leading-6">
-                      $20.00{' '}
-                      <span class="text-red-300 line-through"> $30.00</span>
-                    </p>
-                    <p class="text-base  xl:text-lg leading-6 text-gray-800">
-                      01
-                    </p>
-                    <p class="text-base  xl:text-lg font-semibold leading-6 text-gray-800">
-                      $20.00
-                    </p>
-                  </div>
-                </div>
-              </div>
+                );
+              })}
             </div>
             <div class="flex justify-center  md:flex-row flex-col items-stretch w-full space-y-4 md:space-y-0 md:space-x-6 xl:space-x-8">
               <div class="flex flex-col px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50  space-y-6">
@@ -117,7 +87,9 @@ const Checkout = () => {
                 <div class="flex justify-center items-center w-full space-y-4 flex-col border-gray-200 border-b pb-4">
                   <div class="flex justify-between w-full">
                     <p class="text-base  leading-4 text-gray-800">Subtotal</p>
-                    <p class="text-base  leading-4 text-gray-600">$56.00</p>
+                    <p class="text-base  leading-4 text-gray-600">
+                      ${subtotal}
+                    </p>
                   </div>
                   <div class="flex justify-between items-center w-full">
                     <p class="text-base  leading-4 text-gray-800">
@@ -127,12 +99,12 @@ const Checkout = () => {
                       </span>
                     </p>
                     <p class="text-base  leading-4 text-gray-600">
-                      -$28.00 (50%)
+                      $0.00 &(0%)
                     </p>
                   </div>
                   <div class="flex justify-between items-center w-full">
                     <p class="text-base  leading-4 text-gray-800">Shipping</p>
-                    <p class="text-base  leading-4 text-gray-600">$8.00</p>
+                    <p class="text-base  leading-4 text-gray-600">$0.00</p>
                   </div>
                 </div>
                 <div class="flex justify-between items-center w-full">
@@ -140,13 +112,13 @@ const Checkout = () => {
                     Total
                   </p>
                   <p class="text-base  font-semibold leading-4 text-gray-600">
-                    $36.00
+                    ${subtotal}
                   </p>
                 </div>
               </div>
               <div class="flex flex-col justify-center px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50  space-y-6">
                 <h3 class="text-xl  font-semibold leading-5 text-gray-800">
-                  Shipping
+                  Place A Order - Cash on Delivery
                 </h3>
                 <div class="flex justify-between items-start w-full">
                   <div class="flex justify-center items-center space-x-4">
@@ -171,13 +143,13 @@ const Checkout = () => {
                 </div>
                 <div class="w-full flex justify-center items-center">
                   <button class="hover:bg-black  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 py-5 w-96 md:w-full bg-gray-800 text-base font-medium leading-4 text-white">
-                    View Carrier Details
+                    Place a Order
                   </button>
                 </div>
               </div>
             </div>
           </div>
-          <div class="bg-gray-50  w-full xl:w-96 flex justify-between items-center md:items-start px-4 py-6 md:p-6 xl:p-8 flex-col">
+          <div class="bg-gray-50 rounded-3xl w-full xl:w-96 flex justify-between items-center md:items-start px-4 py-6 md:p-6 xl:p-8 flex-col">
             <h3 class="text-xl  font-semibold leading-5 text-gray-800">
               Customer
             </h3>
@@ -185,12 +157,13 @@ const Checkout = () => {
               <div class="flex flex-col justify-start items-start flex-shrink-0">
                 <div class="flex justify-center w-full md:justify-start items-center space-x-4 py-8 border-b border-gray-200">
                   <img
-                    src="https://i.ibb.co/5TSg7f6/Rectangle-18.png"
+                    src={currentUser.profilePicture}
                     alt="avatar"
+                    className="w-24 h-24 rounded-full"
                   />
                   <div class="flex justify-start items-start flex-col space-y-2">
                     <p class="text-base  font-semibold leading-4 text-left text-gray-800">
-                      David Kent
+                      {currentUser.firstName} {currentUser.lastName}
                     </p>
                     <p class="text-sm  leading-5 text-gray-600">
                       10 Previous Orders
@@ -220,7 +193,7 @@ const Checkout = () => {
                     />
                   </svg>
                   <p class="cursor-pointer text-sm leading-5 ">
-                    david89@gmail.com
+                    {currentUser.email}
                   </p>
                 </div>
               </div>
@@ -228,23 +201,26 @@ const Checkout = () => {
                 <div class="flex justify-center md:justify-start xl:flex-col flex-col md:space-x-6 lg:space-x-8 xl:space-x-0 space-y-4 xl:space-y-12 md:space-y-0 md:flex-row items-center md:items-start">
                   <div class="flex justify-center md:justify-start items-center md:items-start flex-col space-y-4 xl:mt-8">
                     <p class="text-base  font-semibold leading-4 text-center md:text-left text-gray-800">
-                      Shipping Address
+                      Shipping & Billing Address
                     </p>
                     <p class="w-48 lg:w-full  xl:w-48 text-center md:text-left text-sm leading-5 text-gray-600">
-                      180 North King Street, Northhampton MA 1060
+                      {currentUser.address}
                     </p>
                   </div>
                   <div class="flex justify-center md:justify-start items-center md:items-start flex-col space-y-4">
                     <p class="text-base  font-semibold leading-4 text-center md:text-left text-gray-800">
-                      Billing Address
+                      City
                     </p>
                     <p class="w-48 lg:w-full xl:w-48 text-center md:text-left text-sm leading-5 text-gray-600">
-                      180 North King Street, Northhampton MA 1060
+                      {currentUser.city}
                     </p>
                   </div>
                 </div>
                 <div class="flex w-full justify-center items-center md:justify-start md:items-start">
-                  <button class="mt-6 md:mt-0  py-5 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 border border-gray-800 font-medium w-96 2xl:w-full text-base  leading-4 text-gray-800">
+                  <button
+                    onClick={() => navigate('/profile')}
+                    class="mt-6 md:mt-0  py-5 rounded-xl hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 border border-gray-800 font-medium w-96 2xl:w-full text-base  leading-4 text-gray-800"
+                  >
                     Edit Details
                   </button>
                 </div>
